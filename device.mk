4812a620f60b0a7547537797440adc60ce776f9a
ethan-halsall <ethan.halsall@gmail.com>
2019-08-19 16:59:03 +0100

    Bringup syberia
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- device.mk
+++ device.mk
@@ -84,3 +84,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# OTA Updates
+PRODUCT_PACKAGES += \
+    Updates
