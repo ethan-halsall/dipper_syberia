3da172a8553f185e1161a16dd57366bc1369bae2
ethan-halsall <ethan.halsall@gmail.com>
2019-08-29 18:00:56 +0100

    bringup revenge
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- zen_dipper.mk
+++ zen_dipper.mk
@@ -4,13 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Zen OS stuff.
+$(call inherit-product, vendor/zen/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := zen_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
